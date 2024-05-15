485be300a2dd99e37859eb5006354ddd50854d14
abhay3141 <a3141b@gmail.com>
2024-05-15 10:23:30 +0530

    gshddsg

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CVPR_LAB/',
+  base: '/cvpr-lab-iitrpr/',
 })
 
